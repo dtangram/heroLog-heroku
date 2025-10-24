aa3904e615bd6bbe25abe025999592063b7402ad
dtangram <dtangram@yahoo.com>
2025-10-24 13:53:49 -0400

    Made changes in CollectionPublisher controller

--- api/app/controllers/collectionpublishers.ts
+++ api/app/controllers/collectionpublishers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { WhereOptions } from 'sequelize';
+import db from '../models';
 
 // Properly typed model interface
 interface CollectionPublisherModel {
@@ -59,12 +60,7 @@ interface SequelizeError {
   errors: Array<{ message: string }>;
 }
 
-// Import models with proper typing
-const models = require('../models') as {
-  CollectionPublishers: CollectionPublisherModel;
-};
-
-const { CollectionPublishers } = models;
+const CollectionPublishers = db.CollectionPublishers as CollectionPublisherModel;
 
 // Type guard for Sequelize errors
 const isSequelizeError = (error: Error | SequelizeError): error is SequelizeError => {
