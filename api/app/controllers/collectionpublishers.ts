79de3c2026a7f2e84130ce5d696bef70caec1544
dtangram <dtangram@yahoo.com>
2025-10-26 15:47:17 -0400

    Relax UUID validation in controller to accept all UUID formats.

--- api/app/controllers/collectionpublishers.ts
+++ api/app/controllers/collectionpublishers.ts
@@ -69,7 +69,8 @@ const isSequelizeError = (error: Error | SequelizeError): error is SequelizeErro
 
 // UUID validation
 const isValidUUID = (value: string): boolean => {
-  const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  // Accepts any UUID format (8-4-4-4-12 hex characters)
+  const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
   return uuidRegex.test(value);
 };
 
