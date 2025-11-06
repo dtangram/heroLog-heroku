072c19dd081b5c80d1be95989226d23d5f963b23
dtangram <dtangram@yahoo.com>
2025-11-06 12:54:45 -0500

    Google Sign-in

--- reactjs/src/components/forms/signin/Signin.tsx
+++ reactjs/src/components/forms/signin/Signin.tsx
@@ -303,14 +303,6 @@ const handleSubmit = useCallback(
     }, 800);
   }
 
-  // const onSignIn = (googleUser) {
-  //   var profile = googleUser.getBasicProfile();
-  //   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  //   console.log('Name: ' + profile.getName());
-  //   console.log('Image URL: ' + profile.getImageUrl());
-  //   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  // }
-
   return (
     <main id="signin" className={styles.signupMain}>
       <section className={styles.wrapper}>
@@ -327,8 +319,7 @@ const handleSubmit = useCallback(
 
           <fieldset>
             <div className={styles.googleBTN}>
-              <div className="g-signin2" data-onsuccess="onSignIn"></div>
-              {/* <div id="googleSignInButton" style={{ width: '100%' }} /> */}
+              <div id="googleSignInButton" style={{ width: '100%' }} />
             </div>
 
             <section className={styles.orSec}>
