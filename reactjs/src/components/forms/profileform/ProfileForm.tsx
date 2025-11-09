1391e85c2eaf459e7bbe636c53b8959207b71164
dtangram <dtangram@yahoo.com>
2025-11-09 15:00:35 -0500

    Fix ProfileForm to send MIME type instead of file extension.

--- reactjs/src/components/forms/profileform/ProfileForm.tsx
+++ reactjs/src/components/forms/profileform/ProfileForm.tsx
@@ -271,14 +271,11 @@ const ProfileForm = ({ signup, fetchUser, updateUser }: ProfileFormProps) => {
     try {
       console.log('Uploading:', { fileName, fileType });  // Should show "image/jpeg"
 
-      const response = await API.post<{ returnData: { signedRequest: string; url: string } }>(
-        '/sign_s3',
-        { fileName, fileType }
-      );
+      const response = await API.post('/s3/sign', { fileName, fileType });
 
       console.log('S3 sign response:', response);
 
-      const { returnData: { signedRequest, url } } = response.data;
+      const { signedRequest, url } = response.data;
 
       fileInputRef.current.disabled = true;
 
