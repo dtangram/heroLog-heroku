69983b11d022ee5526625fa2a37aea960fe1589c
dtangram <dtangram@yahoo.com>
2025-11-26 10:35:29 -0500

    Fix password reset - use JWT verification and fix syntax errors.

--- api/app/controllers/passwordreset.ts
+++ api/app/controllers/passwordreset.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
-import jwt, { SignOptions } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import { WhereOptions } from 'sequelize';
 import db from '../models';
 
